c39a6c8e11d45a6b4a5ffa9de912c2e36d018013
Mei Jun Chen <wdxlf8992@gmail.com>
2022-09-01 22:15:18 +0800

    Update 2022-09-01T14:15:13.836Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -55,5 +55,28 @@ $(function () {
       delay: 3000
     }
   });
+
+  (function () {
+    Datepicker.locales['zh-TW'] = {
+      days: ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"],
+      daysShort: ["日", "一", "二", "三", "四", "五", "六"],
+      daysMin: ["日", "一", "二", "三", "四", "五", "六"],
+      months: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"],
+      monthsShort: ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"],
+      today: "Today",
+      clear: "Clear",
+      titleFormat: "y年MM月",
+      format: "yyyy/mm/dd",
+      weekStart: 0
+    };
+  })();
+
+  var elem = document.querySelector('input[name="datepicker"]');
+  var datepicker = new Datepicker(elem, {
+    language: 'zh-TW',
+    prevArrow: '<span class="material-icons">keyboard_double_arrow_left</span>',
+    nextArrow: '<span class="material-icons">keyboard_double_arrow_right</span>',
+    autohide: true
+  });
 });
 //# sourceMappingURL=all.js.map
